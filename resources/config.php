7bfab4f8311972b78605050ce0411ab4792535ac
Andela-eugene <eugene.liyai@andela.com>
2018-02-20 23:13:29 +0300

    [Feat] enable output buffering and sessions

--- resources/config.php
+++ resources/config.php
@@ -1,5 +1,9 @@
 <?php 
 
+ob_start();
+session_start();
+
+// directory pattern
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__.DS."templates/front");
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
@@ -11,5 +15,9 @@ defined("DB_PASSWORD") ? null : define("DB_PASSWORD", "");
 defined("DB_NAME") ? null : define("DB_NAME", "");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+require_once("functions.php");
+
+
  
 ?>
