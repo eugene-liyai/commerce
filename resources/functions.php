c28ebff1e3baa9e633818c7b80517b076e5376de
Andela-eugene <eugene.liyai@andela.com>
2018-02-25 16:25:11 +0300

    [Feat] Add product populator function

--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// Helper Functions
+
 function redirect ($location) {
 
 	header('Location: $location ');
@@ -25,4 +27,34 @@ function escape_string ($string) {
 function fetch_array($result) {
 	return mysqli_fetch_array($result);
 }
+
+// Get Products
+
+function get_products() {
+	$query = query('SELECT * FROM products');
+	confirm($query);
+
+	while($row = fetch_array($query)) {
+$product = <<<DELIMETER
+<div class="col-sm-4 col-lg-4 col-md-4">
+    <div class="thumbnail">
+        <a href="item.php?id={$row['product_id']}"><img src="../resources/{$product_image}" alt=""></a>
+        <div class="caption">
+            <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+            </h4>
+            <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+             <a class="btn btn-primary" target="_blank" href="/resources/cart.php?add={$row['product_id']}">Add to cart</a>
+        </div>
+
+
+       
+    </div>
+</div>
+DELIMETER;
+
+	echo $product;
+	}
+}
+
 ?>
