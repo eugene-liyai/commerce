0f1474681e3fbea3465f83be6756195234ad77cb
Andela-eugene <eugene.liyai@andela.com>
2018-02-25 13:26:18 +0300

    [Feat] add css libraries and home page components

--- public/index.php
+++ public/index.php
@@ -9,6 +9,17 @@
 		<!-- Categories -->
 		<?php include(TEMPLATE_FRONT . DS . 'side_nav.php') ?>
 
+		<div class="col-md-9">
+			<div class="row carousel-holder">
+				<div class="col-md-12">
+					
+					<!-- Carousel -->
+					<?php include(TEMPLATE_FRONT . DS . 'slider.php') ?>
+
+				</div>
+			</div>
+		</div>
+
 	</div>
 </div>
 
