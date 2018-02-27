594f80f2f1db561392bbcebcafd6ea148ec3e217
Andela-eugene <eugene.liyai@andela.com>
2018-02-27 23:06:06 +0300

    [Feat] add decrement and delete product from cart functionality

--- public/cart.php
+++ public/cart.php
@@ -18,4 +18,19 @@
 		}
 	}
 
+	if(isset($_GET['remove'])) {
+		$_SESSION['product_'. $_GET['remove']] --;
+
+		if($_SESSION['product_'. $_GET['remove']] < 1) {
+			redirect('checkout.php');
+		} else {
+
+		}
+	}
+
+	if(isset($_GET['delete'])) {
+		$_SESSION['product_'. $_GET['delete']] = 0;
+		redirect('checkout.php');
+	}
+
 ?>
