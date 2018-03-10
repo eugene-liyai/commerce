dc4989f319feca7767d7c86961c81579bc9645f5
Andela-eugene <eugene.liyai@andela.com>
2018-03-10 15:46:12 +0300

    [Feat] add thankyou page

---
+++ public/thank_you.php
@@ -0,0 +1,11 @@
+<?php require_once("../resources/config.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . 'header.php') ?>
+
+<?php //  (); ?>
+  <!-- Page Content -->
+  <div class="container">
+  	<h1 class="text-center">THANK YOU</h1>
+ </div>
+ <!-- /.container -->
+
+<?php include(TEMPLATE_FRONT . DS . 'footer.php') ?>
